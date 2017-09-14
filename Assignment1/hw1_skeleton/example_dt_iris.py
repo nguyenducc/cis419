6ce2e41adb20f4a6a55597994daa3d87a5ad2476
Dziorny <dziornya@chop.edu>
2017-09-14 10:15:27 -0400

    updated assignments

--- Assignment1/hw1_skeleton/example_dt_iris.py
+++ Assignment1/hw1_skeleton/example_dt_iris.py
@@ -34,20 +34,23 @@ iris = load_iris()
 for pairidx, pair in enumerate([[0, 1], [0, 2], [0, 3],
                                 [1, 2], [1, 3], [2, 3]]):
     # We only take the two corresponding features
-    X = iris.data[:, pair]
+    X = iris.data[:, pair]  
     y = iris.target
 
     # Shuffle
-    idx = np.arange(X.shape[0])
-    np.random.seed(13)
-    np.random.shuffle(idx)
-    X = X[idx]
+    # <array>.shape returns the dimensions of the array
+    # arange (start,stop,step) creates a 1-D vector
+    idx = np.arange(X.shape[0])  # returns an array from 0 to X.shape[0], or 150
+    np.random.seed(13)  # Seed the randomization 
+    np.random.shuffle(idx)  # Shuffle in place based on the seeded randomization
+    
+    X = X[idx]   # re-sort X randomly based on IDX as indices
     y = y[idx]
 
     # Standardize
-    mean = X.mean(axis=0)
+    mean = X.mean(axis=0)    # Take the mean over the first axis (long axis)
     std = X.std(axis=0)
-    X = (X - mean) / std
+    X = (X - mean) / std   # Normalize to a 'normal distribution'
 
     # Train
     clf = DecisionTreeClassifier().fit(X, y)
@@ -57,9 +60,14 @@ for pairidx, pair in enumerate([[0, 1], [0, 2], [0, 3],
 
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+    # Meshgrid creates coordinate grid from vectors of coordinates
     xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
                          np.arange(y_min, y_max, plot_step))
 
+    # Ravel creates a 1-D view of the vector
+    # c_ concatenates 1D vectors into columns of a 2D array, basically
+    #    making xx.ravel() column 0, yy.ravel() column 1
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
     cs = plt.contourf(xx, yy, Z, cmap=plt.cm.Paired)
